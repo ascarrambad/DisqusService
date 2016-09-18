a32fbaa11192ac4985d2f7e694d5a3492a06dc29
Matteo Riva <matteoriva@me.com>
2016-09-18 15:54:10 +0200

    Initial Commit

---
+++ DisqusService/DisqusService.h
@@ -0,0 +1,19 @@
+//
+//  DisqusService.h
+//  DisqusService
+//
+//  Created by Matteo Riva on 18/09/2016.
+//  Copyright © 2016 Matteo Riva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DisqusService.
+FOUNDATION_EXPORT double DisqusServiceVersionNumber;
+
+//! Project version string for DisqusService.
+FOUNDATION_EXPORT const unsigned char DisqusServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DisqusService/PublicHeader.h>
+
+
